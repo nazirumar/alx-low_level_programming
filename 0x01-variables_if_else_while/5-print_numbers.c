d16c579a0267dd102004cf1ace6d2d3688763760
Nazir Umar <37006224+nazirumar@users.noreply.github.com>
2022-09-09 18:15:38 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,50 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
+	int num;
 
-while (f < 10)
-{
-	e = 0;
-	while (e < 10)
+
+	for (num = 0; num < 10; num++)
 	{
-		d = 0;
-		while (d < 10)
-		{
-			c = 0;
-			while (c < 10)
-			{
-				if (!(f == c && e == d))
-				{
-					putchar('0' + f);
-					putchar('0' + e);
-					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + c);
-					if (!(f + e == 18 && c + d == 17 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				c++;
-			}
-			d++;
-		}
-		e++;
+		printf("%d", num);
 	}
-	f++;
-}
-putchar('\n');
-return (0);
+
+	printf("\n");
+
+	return (0);
 }
